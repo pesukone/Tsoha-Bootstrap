a46d0eb499fdd572d89636d18537e1dbb9a14f4f
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-03-22 19:38:10 +0200

    tehty näkymät kalenterille, merkinnän luomiselle ja merkinnän näyttämiselle

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -2,14 +2,26 @@
 
   class HelloWorldController extends BaseController{
 
-    public static function index(){
+    public static function index() {
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
       //View::make('home.html');
       echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox(){
+    public static function sandbox() {
       // Testaa koodiasi täällä
       View::make('helloworld.html');
+    }
+
+    public static function calendar_show() {
+      View::make('suunnitelmat/calendar_show.html');
+    }
+
+    public static function event_new() {
+      View::make('suunnitelmat/event_new.html');
+    }
+
+    public static function event_show() {
+      View::make('suunnitelmat/event_show.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,15 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/suunnitelmat/calendar', function() {
+    HelloWorldController::calendar_show();
+  });
+
+  $routes->get('/suunnitelmat/event/new', function() {
+    HelloWorldController::event_new();
+  });
+
+  $routes->get('/suunnitelmat/event/1', function() {
+    HelloWorldController::event_show();
+  });
