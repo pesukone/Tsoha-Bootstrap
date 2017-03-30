54dd8a614418abc9d7cef516bf5a76692c65fc8b
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-03-30 12:42:31 +0300

    tehty näkymä, joka näyttää käyttäjän tapahtumat tietyllä päivällä

--- app/controllers/user_controller.php
+++ app/controllers/user_controller.php
@@ -1,16 +1,16 @@
 <?php
 
-  class UserController extends BaseController{
-    public static function index(){
-      $users = User::all();
+class UserController extends BaseController{
+  public static function index(){
+    $users = User::all();
 
-      View::make('user/index.html', array('users' => $users));
-    }
-
-    public static function list_events($id, $date){
-      $user = User::find($id);
-      $events = Event::list_events($id, $date);
-
-      View::make('user/event_day.html', array('user' => $user, 'events' => $events));
-    }
+    View::make('user/index.html', array('users' => $users));
   }
+
+  public static function list_events($id, $date){
+    $user = User::find($id);
+    $events = Event::list_events($id, $date);
+
+    View::make('user/event_day.html', array('user' => $user, 'events' => $events, 'date' => $date));
+  }
+}
