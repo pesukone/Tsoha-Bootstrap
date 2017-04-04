06dbea0a8ab98e633e3ae0ad611c5af1c097137e
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-04-04 12:05:24 +0300

    tehty validoinnit käyttäjänimelle

--- app/models/user.php
+++ app/models/user.php
@@ -1,43 +1,56 @@
 <?php
 
-  class User extends BaseModel{
+class User extends BaseModel{
 
-    public $id, $name;
+  public $id, $name;
 
-    public function __construct($attributes){
-      parent::__construct($attributes);
-    }
-
-    public static function all(){
-      $query = DB::connection()->prepare('SELECT * FROM Registered');
-      $query->execute();
-      $rows = $query->fetchAll();
-      $users = array();
-
-      foreach($rows as $row){
-        $users[] = new Registered(array(
-	  'id' => $row['id'],
-	  'name' => $row['name']
-	));
-      }
-
-      return $users;
-    }
-
-    public static function find($id){
-      $query = DB::connection()->prepare('SELECT * FROM Registered WHERE id = :id LIMIT 1');
-      $query->execute(array(':id' => $id));
-      $row = $query->fetch();
-
-      if($row){
-        $user = new Registered(array(
-	  'id' => $row['id'],
-	  'name' => $row['name']
-	));
-
-	return $user;
-      }
-
-      return null;
-    }
+  public function __construct($attributes){
+    parent::__construct($attributes);
+    $this->validators = array('validate_name');
   }
+
+  public static function all(){
+    $query = DB::connection()->prepare('SELECT * FROM Registered');
+    $query->execute();
+    $rows = $query->fetchAll();
+    $users = array();
+
+    foreach($rows as $row){
+      $users[] = new Registered(array(
+ 	'id' => $row['id'],
+	'name' => $row['name']
+      ));
+    }
+
+    return $users;
+  }
+
+  public static function find($id){
+    $query = DB::connection()->prepare('SELECT * FROM Registered WHERE id = :id LIMIT 1');
+    $query->execute(array(':id' => $id));
+    $row = $query->fetch();
+
+    if($row){
+      $user = new Registered(array(
+	'id' => $row['id'],
+	'name' => $row['name']
+      ));
+
+      return $user;
+    }
+
+    return null;
+  }
+
+  public function validate_name(){
+    $errors = array();
+    if(!validate_not_null($this->name)){
+      $errors[] = 'Nimi ei saa olla tyhjä!';
+    }
+    if(!validate_string_length($this->name, 3)){
+      $errors[] = 'Nimen pituuden tulee olla vähintään kolme merkkiä!';
+    }
+
+    return $errors;
+  }
+}
