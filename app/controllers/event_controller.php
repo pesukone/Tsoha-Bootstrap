75c58ad6b0b932d00789d30c4c637cebf6e7e3d3
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-04-05 11:52:09 +0300

    tehty merkintäkontrolleriin päivittämis- ja poistamismetodit

--- app/controllers/event_controller.php
+++ app/controllers/event_controller.php
@@ -39,4 +39,40 @@ class EventController extends BaseController{
       View::make('event/new.html', array('errors' => $errors, 'attributes' => $attributes));
     }
   }
+
+  public static function edit($id){
+    $event = Event::find($id);
+    View::make('event/edit.html', array('attributes' => $event));
+  }
+
+  public static function update($id){
+    $params = $_POST;
+
+    $attributes = array(
+      'id' =>$id,
+      'eventday' => $params['day'],
+      'eventtime' => $params['time'],
+      'description' => $params['description'],
+      'user' => $params['user'],
+      'group' => $params['group']
+    );
+
+    $event = new Event($attributes);
+    $errors = $event->errors();
+
+    if(count($errors) > 0){
+      View::make('event/edit.html', array('errors' => $errors, 'attributes' => $attributes));
+    }else{
+      $event->update();
+
+      Redirect::to('/event/' . $event->id, array('message' => 'Tapahtumaa muokattu onnistuneesti!'));
+    }
+  }
+
+  public static function destroy($id){
+    $event = new Event(array('id' => $id));
+    $event->destroy();
+
+    Redirect::to('/event', array('message' => 'Tapahtuma tuhottu onnistuneesti!'));
+  }
 }
